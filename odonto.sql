7a1c1abb72e30b1820867985dd3527514b40e597
robertSantosR <robert-santosrrs@hotmail.com>
2019-06-10 16:22:49 -0300

    banco de dados atualizado

--- odonto.sql
+++ odonto.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 07-Jun-2019 às 02:37
--- Versão do servidor: 10.1.26-MariaDB
--- PHP Version: 7.1.9
+-- Generation Time: 10-Jun-2019 às 21:21
+-- Versão do servidor: 10.1.37-MariaDB
+-- versão do PHP: 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -32,17 +32,25 @@ CREATE TABLE `tb_agendadent` (
   `id_agenda` int(11) NOT NULL,
   `id_funcionarios` int(11) NOT NULL,
   `data` date NOT NULL,
-  `horario` time NOT NULL
+  `horario` time NOT NULL,
+  `tp_status` int(11) NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `tb_agendadent`
 --
 
-INSERT INTO `tb_agendadent` (`id_agenda`, `id_funcionarios`, `data`, `horario`) VALUES
-(22, 6, '2019-05-24', '09:40:00'),
-(23, 7, '1999-03-12', '10:20:00'),
-(24, 7, '1999-03-12', '10:20:00');
+INSERT INTO `tb_agendadent` (`id_agenda`, `id_funcionarios`, `data`, `horario`, `tp_status`, `id_usuario`) VALUES
+(55, 7, '2019-06-08', '14:00:00', 1, 5),
+(56, 6, '2019-06-08', '09:40:00', 1, 5),
+(57, 7, '2019-06-08', '11:00:00', 1, 5),
+(58, 6, '2019-06-08', '11:00:00', 1, 5),
+(59, 7, '2019-06-08', '10:00:00', 1, 5),
+(60, 7, '2019-06-08', '09:20:00', 1, 8),
+(61, 7, '2019-06-08', '11:00:00', 1, 6),
+(62, 7, '2019-06-08', '17:00:00', 2, 6),
+(63, 7, '2019-06-08', '16:20:00', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -63,65 +71,6 @@ CREATE TABLE `tb_cargos` (
 INSERT INTO `tb_cargos` (`id_Cargos`, `nome`, `salario`) VALUES
 (1, 'Auxiliar de dentista', '2000.00'),
 (2, 'DENTISTA SÊNIOR', '14000.00');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tb_consulta`
---
-
-CREATE TABLE `tb_consulta` (
-  `id_Consulta` int(11) NOT NULL,
-  `id_Funcionarios` int(11) NOT NULL,
-  `id_Usuario` int(11) NOT NULL,
-  `id_Procedimento` int(11) NOT NULL,
-  `id_agenda` int(11) DEFAULT NULL,
-  `data` date DEFAULT NULL,
-  `hora` time DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `tb_consulta`
---
-
-INSERT INTO `tb_consulta` (`id_Consulta`, `id_Funcionarios`, `id_Usuario`, `id_Procedimento`, `id_agenda`, `data`, `hora`) VALUES
-(32, 7, 8, 1, 1, NULL, NULL),
-(33, 7, 8, 1, 1, '1970-03-03', NULL),
-(34, 7, 8, 1, 1, '1970-01-01', NULL),
-(36, 7, 8, 1, 1, '1970-01-01', '09:40:00'),
-(37, 7, 8, 1, 1, '1970-01-01', '09:40:00'),
-(38, 7, 8, 1, 2, '1970-01-01', '09:40:00'),
-(42, 7, 8, 1, 5, '2019-05-24', '10:20:00'),
-(43, 7, 8, 1, 6, '1970-01-01', '09:40:00'),
-(44, 7, 8, 1, 9, '2019-05-24', '10:00:00'),
-(49, 6, 8, 1, 10, '2019-05-24', '09:20:00'),
-(50, 6, 8, 1, 11, '2019-05-24', '09:20:00'),
-(51, 6, 8, 1, 11, '2019-05-24', '09:20:00'),
-(52, 6, 8, 1, 11, '2019-05-24', '09:20:00'),
-(53, 6, 8, 1, 11, '2019-05-24', '09:20:00'),
-(54, 6, 8, 1, 11, '2019-05-24', '09:20:00'),
-(55, 6, 8, 1, 11, '2019-05-24', '09:20:00'),
-(56, 6, 8, 1, 11, '2019-05-24', '09:20:00'),
-(57, 6, 8, 1, 12, '2019-05-24', '09:20:00'),
-(58, 6, 8, 1, 12, '2019-05-24', '09:20:00'),
-(59, 6, 8, 1, 12, '2019-05-24', '09:20:00'),
-(60, 6, 8, 1, 13, '2019-05-24', '09:20:00'),
-(61, 6, 8, 1, 13, '2019-05-24', '09:20:00'),
-(62, 6, 8, 1, 13, '2019-05-24', '09:20:00'),
-(63, 6, 8, 1, 13, '2019-05-24', '09:20:00'),
-(64, 6, 8, 1, 13, '2019-05-24', '09:20:00'),
-(65, 6, 8, 1, 13, '2019-05-24', '09:20:00'),
-(66, 6, 8, 1, 13, '2019-05-24', '09:20:00'),
-(67, 6, 8, 1, 14, '1970-01-01', '09:20:00'),
-(68, 6, 8, 1, 14, '1970-01-01', '09:20:00'),
-(69, 6, 8, 1, 15, '1970-01-01', '09:20:00'),
-(70, 6, 8, 1, 16, '1970-01-01', '09:20:00'),
-(71, 6, 8, 1, 16, '1970-01-01', '09:20:00'),
-(72, 6, 8, 1, 17, '1970-01-01', '09:20:00'),
-(73, 6, 8, 1, 18, '1970-01-01', '09:20:00'),
-(74, 6, 8, 1, 19, '1970-01-01', '09:20:00'),
-(75, 6, 8, 1, 20, '2019-05-24', '09:40:00'),
-(76, 6, 8, 1, 21, '2019-05-24', '09:40:00');
 
 -- --------------------------------------------------------
 
@@ -150,26 +99,8 @@ INSERT INTO `tb_funcionarios` (`id_Funcionarios`, `id_Cargos`, `Funcionario`, `d
 (4, 1, 'robert', '2019-05-17', '41395253', '1194667885', '394800874', '49184667885', '202cb962ac59075b964b07152d234b70'),
 (5, 1, 'robert', '2019-05-17', '41395253', '1194667885', '394800874', '49184667885', '202cb962ac59075b964b07152d234b70'),
 (6, 2, 'robert', '2019-05-08', '41395253', '1194667885', '1234', '49184667885', '202cb962ac59075b964b07152d234b70'),
-(7, 2, 'joseCirurgiao', '2019-05-08', '41395253', '1194667885', '396544896', '48831767885', '202cb962ac59075b964b07152d234b70');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tb_procedimento`
---
-
-CREATE TABLE `tb_procedimento` (
-  `id_Procedimento` int(11) NOT NULL,
-  `Procedimento` varchar(20) NOT NULL,
-  `Valor` decimal(10,0) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `tb_procedimento`
---
-
-INSERT INTO `tb_procedimento` (`id_Procedimento`, `Procedimento`, `Valor`) VALUES
-(1, 'Limpeza', '150');
+(7, 2, 'joseCirurgiao', '2019-05-08', '41395253', '1194667885', '396544896', '48831767885', '202cb962ac59075b964b07152d234b70'),
+(8, 2, 'vagner', '2019-06-24', '1141395253', '11974455879', '354677895', '49887445667', '202cb962ac59075b964b07152d234b70');
 
 -- --------------------------------------------------------
 
@@ -183,27 +114,29 @@ CREATE TABLE `tb_usuario` (
   `Email` varchar(25) NOT NULL,
   `Telefone` varchar(15) NOT NULL,
   `Celular` varchar(15) NOT NULL,
-  `senha` varchar(20) DEFAULT NULL,
   `endereco` varchar(100) NOT NULL,
   `cep` varchar(100) NOT NULL,
   `numero` varchar(100) NOT NULL,
   `complemento` varchar(100) NOT NULL,
-  `cpf` varchar(15) NOT NULL
+  `cpf` varchar(15) NOT NULL,
+  `senha` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `tb_usuario`
 --
 
-INSERT INTO `tb_usuario` (`id_Usuario`, `paciente`, `Email`, `Telefone`, `Celular`, `senha`, `endereco`, `cep`, `numero`, `complemento`, `cpf`) VALUES
-(1, 'dd', 'erew', '112314', '1147013758', NULL, 'rua helena', '06767390 ', 'dsf', '11645', '39166978890'),
-(2, 'a', 'a', '112314', '', NULL, '', ' ', '', '', ''),
-(3, 'a', 'b@ocm.c', '112314', '', NULL, '', ' ', '', '', ''),
-(4, 'a', 'ab@ab.com', '112314', '', NULL, '', ' ', '', '', ''),
-(5, 'elias', 'elias@hotmail.com', '', '', '123', '', '', '', '', ''),
-(6, 'robert', 'robert@hotmail.com', '112314', '1194667885', NULL, 'rua ananias carmerindo pires', '06785100 ', '405', 'c2', '49184667885'),
-(7, 'vagner', 'vagner@hotmail.com', '112314', '1194667885', NULL, 'rua ananias carmerindo pires', '06785100 ', '405', 'c2', '49184667885'),
-(8, 'robert', 'robert@hotmail.com', '112314', '1194667885', '123', 'rua ananias carmerindo pires', '06785100 ', '405', 'c2', '49184667885');
+INSERT INTO `tb_usuario` (`id_Usuario`, `paciente`, `Email`, `Telefone`, `Celular`, `endereco`, `cep`, `numero`, `complemento`, `cpf`, `senha`) VALUES
+(1, 'dd', 'erew', '112314', '1147013758', 'rua helena', '06767390 ', 'dsf', '11645', '39166978890', ''),
+(2, 'a', 'a', '112314', '', '', ' ', '', '', '', ''),
+(3, 'a', 'b@ocm.c', '112314', '', '', ' ', '', '', '', ''),
+(4, 'a', 'ab@ab.com', '112314', '', '', ' ', '', '', '', ''),
+(5, 'elias', 'elias@hotmail.com', '', '', '', '', '', '', '', '202cb962ac59075b964b07152d234b70'),
+(6, 'robert', 'robert@hotmail.com', '112314', '1194667885', 'rua ananias carmerindo pires', '06785100 ', '405', 'c2', '49184667885', '202cb962ac59075b964b07152d234b70'),
+(7, 'vagner', 'vagner@hotmail.com', '112314', '1194667885', 'rua ananias carmerindo pires', '06785100 ', '405', 'c2', '49184667885', ''),
+(8, 'robert', 'robert@hotmail.com', '112314', '1194667885', 'rua ananias carmerindo pires', '06785100 ', '405', 'c2', '49184667885', ''),
+(9, '', '', '', '', '', '', '', '', '', '202cb962ac59075b964b07152d234b70'),
+(10, 'vagner', 'vagner@hotmail.com', '112314', '11974455879', 'jd senac', '06785125 ', '403', 'c2', '49887445667', '202cb962ac59075b964b07152d234b70');
 
 --
 -- Indexes for dumped tables
@@ -214,7 +147,8 @@ INSERT INTO `tb_usuario` (`id_Usuario`, `paciente`, `Email`, `Telefone`, `Celula
 --
 ALTER TABLE `tb_agendadent`
   ADD PRIMARY KEY (`id_agenda`),
-  ADD KEY `id_funcionarios` (`id_funcionarios`);
+  ADD KEY `id_funcionarios` (`id_funcionarios`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indexes for table `tb_cargos`
@@ -223,26 +157,11 @@ ALTER TABLE `tb_cargos`
   ADD PRIMARY KEY (`id_Cargos`);
 
 --
--- Indexes for table `tb_consulta`
---
-ALTER TABLE `tb_consulta`
-  ADD PRIMARY KEY (`id_Consulta`),
-  ADD KEY `fk_tb_Consulta_id_Usuario` (`id_Usuario`),
-  ADD KEY `fk_tb_Consulta_id` (`id_Funcionarios`),
-  ADD KEY `fk_tb_Consulta_id_Procedimento` (`id_Procedimento`);
-
---
 -- Indexes for table `tb_funcionarios`
 --
 ALTER TABLE `tb_funcionarios`
   ADD PRIMARY KEY (`id_Funcionarios`),
   ADD KEY `fk_tb_Funcionarios_id` (`id_Cargos`);
-
---
--- Indexes for table `tb_procedimento`
---
-ALTER TABLE `tb_procedimento`
-  ADD PRIMARY KEY (`id_Procedimento`);
 
 --
 -- Indexes for table `tb_usuario`
@@ -258,7 +177,7 @@ ALTER TABLE `tb_usuario`
 -- AUTO_INCREMENT for table `tb_agendadent`
 --
 ALTER TABLE `tb_agendadent`
-  MODIFY `id_agenda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id_agenda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 
 --
 -- AUTO_INCREMENT for table `tb_cargos`
@@ -267,28 +186,16 @@ ALTER TABLE `tb_cargos`
   MODIFY `id_Cargos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `tb_consulta`
---
-ALTER TABLE `tb_consulta`
-  MODIFY `id_Consulta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
-
---
 -- AUTO_INCREMENT for table `tb_funcionarios`
 --
 ALTER TABLE `tb_funcionarios`
-  MODIFY `id_Funcionarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `tb_procedimento`
---
-ALTER TABLE `tb_procedimento`
-  MODIFY `id_Procedimento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_Funcionarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `tb_usuario`
 --
 ALTER TABLE `tb_usuario`
-  MODIFY `id_Usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_Usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
@@ -298,15 +205,8 @@ ALTER TABLE `tb_usuario`
 -- Limitadores para a tabela `tb_agendadent`
 --
 ALTER TABLE `tb_agendadent`
+  ADD CONSTRAINT `id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `tb_usuario` (`id_Usuario`),
   ADD CONSTRAINT `tb_agendadent_ibfk_1` FOREIGN KEY (`id_funcionarios`) REFERENCES `tb_funcionarios` (`id_Funcionarios`);
-
---
--- Limitadores para a tabela `tb_consulta`
---
-ALTER TABLE `tb_consulta`
-  ADD CONSTRAINT `fk_tb_Consulta_id` FOREIGN KEY (`id_Funcionarios`) REFERENCES `tb_funcionarios` (`id_Funcionarios`),
-  ADD CONSTRAINT `fk_tb_Consulta_id_Procedimento` FOREIGN KEY (`id_Procedimento`) REFERENCES `tb_procedimento` (`id_Procedimento`),
-  ADD CONSTRAINT `fk_tb_Consulta_id_Usuario` FOREIGN KEY (`id_Usuario`) REFERENCES `tb_usuario` (`id_Usuario`);
 
 --
 -- Limitadores para a tabela `tb_funcionarios`
